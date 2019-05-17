181c13ad00b0b4c222d10fa840f420c4b0d65841
AshwiniBathula <49541481+AshwiniBathula@users.noreply.github.com>
2019-05-17 16:14:42 +1000

    Update ListingManagement.cs

--- MarsFramework/Pages/ListingManagement.cs
+++ MarsFramework/Pages/ListingManagement.cs
@@ -1,4 +1,4 @@
-﻿using OpenQA.Selenium;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Support.PageObjects;
 using RelevantCodes.ExtentReports;
 using System;
@@ -36,23 +36,63 @@ namespace MarsFramework.Pages
         [FindsBy(How = How.XPath, Using = "//table//tbody//tr//td[3][@class='two wide']")]
         private IList<IWebElement> Title { get; set; }
 
+        [FindsBy(How = How.XPath, Using = "//button[@class='ui icon positive right labeled button']")]
+        private IWebElement ModalYesButton { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//button[@class='ui negative button']")]
+        private IWebElement ModalNoButton { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//h3[contains(text(),'You do not have any service listings!')]")]
+        private IWebElement NoListing { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//button[@role='button' and not(contains(text(),'>')) and not(contains(text(),'<'))]")]
+        private IList<IWebElement> NoOfPages { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//button[ contains(text(),'>')]")]
+        private IWebElement NextPage { get; set; }
+
+
         public void ViewServiceDetails()
         {
-            Global.GlobalDefinitions.ExcelLib.PopulateInCollection(Global.Base.ExcelPath,"ListingManagement");
+            Global.GlobalDefinitions.ExcelLib.PopulateInCollection(Global.Base.ExcelPath, "ListingManagement");
             ManageListing.Click();
-            Base.test.Log(LogStatus.Info, "Starting Viewing Service");
-            Global.GlobalDefinitions.wait(2000);
-            
-            for (int i = 0; i < Title.Count; i++)
+            Base.test.Log(LogStatus.Info, "Starting editing Service");
+            Global.GlobalDefinitions.wait(5000);
+            bool RecordFound = false;
+
+            try
             {
-                ManageListing.Click();
-                if ((Global.GlobalDefinitions.ExcelLib.ReadData(2, "Title") == Title[i].Text) && (Global.GlobalDefinitions.ExcelLib.ReadData(2, "Category") == Category[i].Text))
+                for (int p = 0; p < NoOfPages.Count; p++)
                 {
-                    view[i].Click();
-                    Console.WriteLine(i);
-                    Global.GlobalDefinitions.wait(2000);
+                    for (int i = 0; i < Title.Count; i++)
+                    {
+                        if ((Global.GlobalDefinitions.ExcelLib.ReadData(2, "Title") == Title[i].Text) && (Global.GlobalDefinitions.ExcelLib.ReadData(2, "Category") == Category[i].Text))
+                        {
+                            RecordFound = true;
+                            view[i].Click();
+                            Global.GlobalDefinitions.wait(2000);
+                            ManageListing.Click();
+                        }
+                    }
+                    if (NextPage.Enabled)
+                        NextPage.Click();
+                    else if (RecordFound == false)
+                    {
+                        Global.Base.test.Log(LogStatus.Info, "There are no matching listings!");
+                    }
                 }
             }
+            catch (Exception e)
+            {
+                IWebElement NoListings = Global.GlobalDefinitions.driver.FindElement(By.XPath("//h3[contains(text(),'You do not have any service listings!')]"));
+                if (NoListings.Displayed)
+                {
+                    Global.Base.test.Log(LogStatus.Info, "You do not have any service listings to display");
+                }
+                else
+                    Global.Base.test.Log(LogStatus.Info, "Something went wrong!" + e);
+            }
+           
         }
 
         public void EditServiceDetails()
@@ -60,17 +100,41 @@ namespace MarsFramework.Pages
             Global.GlobalDefinitions.ExcelLib.PopulateInCollection(Global.Base.ExcelPath, "ListingManagement");
             ManageListing.Click();
             Base.test.Log(LogStatus.Info, "Starting editing Service");
-            Global.GlobalDefinitions.wait(2000);
-
-            for (int i = 0; i < Title.Count; i++)
+            Global.GlobalDefinitions.wait(5000);
+            bool RecordFound = false;
+            try
             {
-                if ((Global.GlobalDefinitions.ExcelLib.ReadData(2, "Title") == Title[i].Text) && (Global.GlobalDefinitions.ExcelLib.ReadData(2, "Category") == Category[i].Text))
-                {
-                    Edit[i].Click();
-                    ServiceListing service = new ServiceListing();
-                    service.AddService();
-                    Global.GlobalDefinitions.wait(2000);
+                for (int p = 0; p < NoOfPages.Count; p++)
+                { 
+                    for (int i = 0; i < Title.Count; i++)
+                    {
+                        if ((Global.GlobalDefinitions.ExcelLib.ReadData(2, "Title") == Title[i].Text) && (Global.GlobalDefinitions.ExcelLib.ReadData(2, "Category") == Category[i].Text))
+                        {
+                            RecordFound = true;
+                            Edit[i].Click();
+                            ServiceListing service = new ServiceListing();
+                            service.AddService();
+                            Global.GlobalDefinitions.wait(2000);
+                            i = i + 1;
+                        }   
+                    }
+                    if (NextPage.Enabled)
+                        NextPage.Click();
+                    else if(RecordFound == false)
+                    {
+                        Global.Base.test.Log(LogStatus.Info, "There are no matching listings!");
+                    }
                 }
+            }
+            catch (Exception e)
+            {
+                IWebElement NoListings = Global.GlobalDefinitions.driver.FindElement(By.XPath("//h3[contains(text(),'You do not have any service listings!')]"));
+                if (NoListings.Displayed)
+                {
+                    Global.Base.test.Log(LogStatus.Info, "You do not have any service listings to display");
+                }
+                else
+                    Global.Base.test.Log(LogStatus.Info, "Something went wrong!" + e);
             }
         }
 
@@ -80,34 +144,76 @@ namespace MarsFramework.Pages
             Global.GlobalDefinitions.wait(2000);
             ManageListing.Click();
             Base.test.Log(LogStatus.Info, "Starting removing Service");
-            Global.GlobalDefinitions.wait(2000);
-            //Console.WriteLine("title countis:" + Title.Count);
+            Global.GlobalDefinitions.wait(5000);
+            bool RecordFound = false;
             int flag = 0;
 
-            if (NoListing.Displayed)
+            //try
+            //{
+            //    for (int p = 0; p < NoOfPages.Count; p++)
+            //    {
+            //        for (int i = 0; i < Title.Count; i++)
+            //        {
+            //            Console.WriteLine("i value:" + i);
+            //            if ((Global.GlobalDefinitions.ExcelLib.ReadData(2, "Title") == Title[i].Text) && (Global.GlobalDefinitions.ExcelLib.ReadData(2, "Category") == Category[i].Text))
+            //            {
+            //                RecordFound = true;
+            //                WebDriverWait wait = new WebDriverWait(Global.GlobalDefinitions.driver, TimeSpan.FromSeconds(10));
+            //                wait.Until(ExpectedConditions.ElementToBeClickable((Remove[i])));
+            //                Remove[i].Click();
+            //                Console.WriteLine("removing row:"+i);
+            //                Global.GlobalDefinitions.wait(2000);
+            //                ModalYesButton.Click();
+            //                i = i - 1;
+            //            }
+            //        }
+            //        if (NextPage.Enabled)
+            //            NextPage.Click();
+            //        else if (RecordFound == false)
+            //        {
+            //            Global.Base.test.Log(LogStatus.Info, "There are no matching listings!");
+            //        }
+            //    }
+            //}
+            try
             {
-                Global.Base.test.Log(LogStatus.Info, "There are no services to remove");
-            }
-            else
-            {
-                for (int i = 0; i < Title.Count;)
+                for (int p = 0; p < NoOfPages.Count; p++)
                 {
-                    if (flag == Title.Count)
-                        break;
-                    else if ((Global.GlobalDefinitions.ExcelLib.ReadData(2, "Title") == Title[i].Text) && (Global.GlobalDefinitions.ExcelLib.ReadData(2, "Category") == Category[i].Text))
+                    for (int i = 0; i < Title.Count;i++)
                     {
-                        Console.WriteLine("i value:" + i);
-                        WebDriverWait wait = new WebDriverWait(Global.GlobalDefinitions.driver, TimeSpan.FromSeconds(10));
-                        wait.Until(ExpectedConditions.ElementToBeClickable((Remove[i])));
-                        Remove[i].Click();
-                        ModalYesButton.Click();
-                        Global.GlobalDefinitions.wait(2000);
-                        i = 0;
-                        flag++;
+                        Global.GlobalDefinitions.wait(7000);
+                        if (flag == Title.Count)
+                            break;
+                        else
+                        if ((Global.GlobalDefinitions.ExcelLib.ReadData(2, "Title") == Title[i].Text) && (Global.GlobalDefinitions.ExcelLib.ReadData(2, "Category") == Category[i].Text))
+                        {
+                            RecordFound = true;
+                            WebDriverWait wait = new WebDriverWait(Global.GlobalDefinitions.driver, TimeSpan.FromSeconds(10));
+                            wait.Until(ExpectedConditions.ElementToBeClickable((Remove[i])));
+                            Remove[i].Click();
+                            ModalYesButton.Click();
+                            Global.GlobalDefinitions.wait(2000);
+                            i = i-1;
+                            flag++;
+                        }
                     }
-                    else
-                        i++;
+                    if (NextPage.Enabled)
+                        NextPage.Click();
+                    else if (RecordFound == false)
+                    {
+                        Global.Base.test.Log(LogStatus.Info, "There are no matching listings!");
+                    }
                 }
+            }
+            catch (Exception e)
+            {
+                IWebElement NoListings = Global.GlobalDefinitions.driver.FindElement(By.XPath("//h3[contains(text(),'You do not have any service listings!')]"));
+                if (NoListing.Displayed)
+                {
+                    Global.Base.test.Log(LogStatus.Info, "There are no services to remove");
+                }
+                else
+                    Global.Base.test.Log(LogStatus.Info, "Something went wrong!" + e);
             }
         }
 
@@ -117,18 +223,30 @@ namespace MarsFramework.Pages
             Global.GlobalDefinitions.wait(2000);
             ManageListing.Click();
             Base.test.Log(LogStatus.Info, "Starting removing Service");
-            Global.GlobalDefinitions.wait(2000);
-            Console.WriteLine("remove countis:" + Remove.Count);
-
-            foreach(IWebElement ele in Remove)
+            Global.GlobalDefinitions.wait(5000);
+            
+            try
             {
-                //Console.WriteLine("i value:" + i);
-                //WebDriverWait wait = new WebDriverWait(Global.GlobalDefinitions.driver, TimeSpan.FromSeconds(10));
-                //wait.Until(ExpectedConditions.ElementToBeClickable((Remove[i])));
-                ele.Click();
-                ModalYesButton.Click();
-                Global.GlobalDefinitions.wait(2000);
+                foreach (IWebElement ele in Remove)
+                { 
+                    WebDriverWait wait = new WebDriverWait(Global.GlobalDefinitions.driver, TimeSpan.FromSeconds(10));
+                    wait.Until(ExpectedConditions.ElementToBeClickable((ele)));
+                    ele.Click();
+                    ModalYesButton.Click();
+                    Global.GlobalDefinitions.wait(5000);
+                }
             }
+            catch (Exception e)
+            {
+                IWebElement NoListings = Global.GlobalDefinitions.driver.FindElement(By.XPath("//h3[contains(text(),'You do not have any service listings!')]"));
+                if (NoListing.Displayed)
+                {
+                    Global.Base.test.Log(LogStatus.Info, "There are no services to remove");
+                }
+                else
+                    Global.Base.test.Log(LogStatus.Info, "Something went wrong!" + e);
+            }
+           
         }
     }
 }
